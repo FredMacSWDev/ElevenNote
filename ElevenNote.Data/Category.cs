25c05a35733acc5f839cec2c88373abcc7a9ba4a
Frederick McClung <Frederick.McClung.SWDev@gmail.com>
2021-05-28 10:35:45 -0400

    Added 'Category' class

---
+++ ElevenNote.Data/Category.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public string ProductName { get; set; }
+        
+        [Required]
+        public string Occasion { get; set; }
+
+        [Required]
+        [Range(0,double.MaxValue, ErrorMessage = "The price must be greater than 0")]
+        public double Price { get; set; }
+        
+        [Required]
+        public int StockQuantity { get; set; }
+
+        
+        public DateTimeOffset? CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
